b86ff06165c65c4ddf21fbaffbddc68e0b0e7b15
Victoregb <67790915+psiconomicon@users.noreply.github.com>
2021-03-16 15:55:42 +0100

    comentarios añadidos

--- SQL_02.py
+++ SQL_02.py
@@ -1,9 +1,17 @@
+'''
+Estos ejercicios estan sacados de SQL Head First.
+Como actividad introductoria al SQL.
+Habría que comprobar si añadir un ; a todas las instrucciones se considera
+Zen o no.
+'''
 #%% 
 import sqlite3
 import pandas as pd
 #%%
 conect = sqlite3.connect('base_datos.db')
 cursor = conect.cursor()
+
+#Buscar que es PRAGMA.
 cursor.execute('PRAGMA table_info(gregs_list)')
 # %%
 
@@ -44,7 +52,8 @@ conect.commit()
 
 cursor.execute('''INSERT INTO gregs_list 
                 VALUES ('Paquez', 'Paco', 'Varón', 'cuatro@dos.tres', 
-                        '16/06/1688', 'analista', 'Valladolid', 'Casado', 'No pasar hambre', 'Trabajo')
+                        '16/06/1688', 'analista', 'Valladolid', 'Casado', 
+                        'No pasar hambre', 'Trabajo')
                
                ;''')
 conect.commit()
@@ -67,7 +76,8 @@ datos
 
 # %%
 df = pd.DataFrame(datos, columns=['apellido', 'nombre', 'genero', 'correo',
-                                  'cumpleaños', 'profesión', 'ciudad', 'estado', 'intereses', 'buscando'])
+                                  'cumpleaños', 'profesión', 'ciudad', 'estado', 
+                                  'intereses', 'buscando'])
 df
 
 # %%
@@ -75,9 +85,25 @@ conect = sqlite3.connect('base_datos.db')
 cursor = conect.cursor()
 
 # %%
-cursor.execute("INSERT INTO ciudad VALUES('Valladolid', 350000, 'Valladolid')")
+conect = sqlite3.connect('base_datos.db')
+cursor = conect.cursor()
+#El error cometido es tratar de crear una tabla sin tener en cuenta
+# que hace falta tambien declarar las columnas.
+cursor.execute("""CREATE TABLE ciudad(nombre, población, provincia);
+               """)
+conect.commit()
+
+# %%
+cursor.execute("INSERT INTO ciudad VALUES('Valladolid', 350000, 'Valladolid');")
 conect.commit()
 conect = sqlite3.connect('base_datos.db')
 cursor = conect.cursor()
+
+
+# %%
+cursor.execute('SELECT * FROM ciudad')
+# %%
+datos = cursor.fetchall()
+datos
 
 # %%
